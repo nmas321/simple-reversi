bb4b9e53a3c1504a66824dee54b6c76d3dae1252
Nakano Masashi <nmas321@gmail.com>
2023-12-16 16:35:12 +0900

    1st implementation reversi

---
+++ components/Reversi.ts
@@ -0,0 +1,124 @@
+type Turn = 'b' | 'w';
+type SquareState = Turn | '-' | 'h';
+
+export class Reversi {
+    private Direction = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, 1],
+        [0, -1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+      ];
+
+  private canPutDirection(rc: [number, number], i: number, j: number): boolean {
+    let x = rc[0] + i;
+    let y = rc[1] + j;
+    if(!this.within(x, y)) return false;
+    
+    while(this.within(x, y) && this.board[x][y] === this.b(this.turn))
+    {
+        x += i;
+        y += j;
+    }
+
+    return !((x === (rc[0] + i) && y === (rc[1] + j)) || !this.within(x, y) ||
+        this.board[x][y] != this.turn);
+  }
+
+  public canPut(rc: [number, number]): boolean {
+    if (this.board[rc[0]][rc[1]] != '-') {
+      return false;
+    }
+    for (const d of this.Direction) {
+      if (this.canPutDirection(rc, d[0], d[1])) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  private within(x: number, y: number)
+  {
+    return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+  }
+
+  private putDirection(rc: [number, number], i: number, j: number): void {
+    let x = rc[0] + i;
+    let y = rc[1] + j;
+    if(!this.within(x, y)) return;
+
+    while(this.within(x, y) && this.board[x][y] === this.b(this.turn))
+    {
+        x += i;
+        y += j;
+    }
+
+    if((x === (rc[0] + i) && y === (rc[1] + j)) || !this.within(x, y) ||
+        this.board[x][y] != this.turn)
+    {
+        return;
+    }
+
+    x -= i;
+    y -= j;
+
+    while(x !== rc[0] || y !== rc[1])
+    {
+        this.board[x][y] = this.turn;
+        x -= i;
+        y -= j;
+    }
+  }
+
+  put(rc: [number, number]): boolean {
+    if (!this.canPut(rc)) {
+      return false;
+    }
+    this.board[rc[0]][rc[1]] = this.turn;
+    this.Direction.forEach((v) => this.putDirection(rc, v[0], v[1]));
+    this.turn = this.b(this.turn);
+    return true;
+  }
+
+  isUserTurn() {
+    return this.turn === "b";
+  }
+
+  private board: SquareState[][] = [
+    ['-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', 'w', 'b', '-', '-', '-'],
+    ['-', '-', '-', 'b', 'w', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-'],
+    ['-', '-', '-', '-', '-', '-', '-', '-'],
+  ];
+  private turn: Turn = 'b';
+
+  private b(turn: Turn): Turn 
+  {
+    return turn === 'b'? 'w' : 'b';
+  }
+
+  public getBoard(): SquareState[][] {
+    return this.board;
+  }
+
+  public getHintBoard(): SquareState[][] {
+    const hintBoard = structuredClone(this.board);
+
+    [...Array(8).keys()].forEach((i) => 
+    {
+        [...Array(8).keys()].forEach((j) => {
+            if(this.canPut([i, j])) {
+                hintBoard[i][j] = 'h';
+            }
+        });
+    });
+    return hintBoard;
+  }
+}
---
+++ islands/Board.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useRef, useState } from "preact/hooks";
+import { Reversi } from "../components/Reversi.ts";
+import { RefactorEditInfo } from "https://deno.land/x/ts_morph@20.0.0/ts_morph.js";
+
+const reversi = new Reversi();
+
+const BoardItemSize = 72;
+const BoardSize = BoardItemSize * 8;
+
+function getRowCol(ev: MouseEvent): [number, number] {
+  const canvas = ev.target as HTMLCanvasElement;
+  const rect = canvas.getBoundingClientRect();
+  const x = ev.clientX - rect.left;
+  const y = ev.clientY - rect.top;
+
+  return [Math.floor(x / (rect.width / 8)), Math.floor(y / (rect.height / 8))];
+}
+
+export function Board() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  function onclick(ev: MouseEvent): any {
+    // if (!reversi.isUserTurn()) {
+    //   return;
+    // }
+  
+    reversi.put(getRowCol(ev));
+    setIsDrawing(true);
+  }
+  
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvasRef.current?.getContext("2d");
+    if (!canvas || !context) return;
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    canvas.style.margin = "10px";
+    canvas.width = BoardSize;
+    canvas.height = BoardSize;
+    const b = reversi.getHintBoard();
+    drawBoard(b, context);
+    canvas.onclick = onclick;
+    setIsDrawing(false);
+  }, [isDrawing]);
+
+  return <canvas ref={canvasRef}></canvas>;
+}
+
+function drawBoard(board: string[][], context: CanvasRenderingContext2D) {
+  context.fillStyle = "#008000";
+  context.strokeStyle = "#000000";
+  context.fillRect(0, 0, BoardSize, BoardSize);
+  context.strokeRect(0, 0, BoardSize, BoardSize);
+  for (let i = 0; i < 8; ++i) {
+    for (let j = 0; j < 8; ++j) {
+      drawBoardItem(i * BoardItemSize, j * BoardItemSize, board[i][j], context);
+    }
+  }
+}
+
+function drawBoardItem(
+  x: number,
+  y: number,
+  item: string,
+  context: CanvasRenderingContext2D,
+) {
+  context.fillStyle = "#008000";
+  context.strokeStyle = "#000000";
+  context.strokeRect(x, y, BoardItemSize, BoardItemSize);
+
+  if (item == "b" || item == "w") {
+    context.beginPath();
+    context.arc(
+      x + BoardItemSize / 2,
+      y + BoardItemSize / 2,
+      BoardItemSize / 2 - 5,
+      0,
+      Math.PI * 2,
+    );
+    context.fillStyle = item == "w" ? "white" : "black";
+    context.fill();
+    context.strokeStyle = item == "w" ? "black" : "white";
+    context.lineWidth = 1;
+    context.stroke();
+  }
+  
+  if (item == "h") {
+    context.beginPath();
+    context.arc(
+      x + BoardItemSize / 2,
+      y + BoardItemSize / 2,
+      BoardItemSize / 2 - 20,
+      0,
+      Math.PI * 2,
+    );
+    context.fillStyle = 'green'
+    context.fill();
+    context.strokeStyle = 'black';
+    context.lineWidth = 1;
+    context.stroke();
+  }
+}
